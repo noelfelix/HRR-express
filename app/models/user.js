df44d34a4238012a1e7f1c46a3e4bba44635b951
Noel Felix <nfelix83@gmail.com>
2016-02-09 19:16:25 -0800

    Completed

---
+++ app/models/user.js
@@ -0,0 +1,16 @@
+var db = require('../config');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function(){
+    this.on('creating', function(model, attrs, options){
+      var password = model.get('password');
+      var hash = bcrypt.hashSync(password);
+      model.set('password', hash);
+    });
+  }
+});
+
+module.exports = User;
---
+++ shortly.js
@@ -0,0 +1,190 @@
+var express = require('express');
+var util = require('./lib/utility');
+var partials = require('express-partials');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt-nodejs');
+var session = require('express-session');
+
+
+var db = require('./app/config');
+var Users = require('./app/collections/users');
+var User = require('./app/models/user');
+var Links = require('./app/collections/links');
+var Link = require('./app/models/link');
+var Click = require('./app/models/click');
+var User = require('./app/models/user');
+
+var app = express();
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(partials());
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
+// Parse forms (signup/login)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+var authenticate = function(request, response){
+  console.log('req.session.user: ', request.session.user);
+  if (request.session.user){
+  } else {
+    response.redirect('/login');
+  }
+};
+
+app.get('/', 
+function(req, res) {
+  authenticate(req, res);
+  res.render('index');
+});
+
+app.get('/create', 
+function(req, res) {
+  authenticate(req, res);
+  res.render('index');
+});
+
+app.get('/links', 
+function(req, res) {
+  authenticate(req, res);
+  Links.reset().fetch().then(function(links) {
+    res.send(200, links.models);
+  });
+});
+
+app.post('/links', 
+function(req, res) {
+  var uri = req.body.url;
+
+  if (!util.isValidUrl(uri)) {
+    console.log('Not a valid url: ', uri);
+    return res.send(404);
+  }
+
+  new Link({ url: uri }).fetch().then(function(found) {
+    if (found) {
+      res.send(200, found.attributes);
+    } else {
+      util.getUrlTitle(uri, function(err, title) {
+        if (err) {
+          console.log('Error reading URL heading: ', err);
+          return res.send(404);
+        }
+
+        var link = new Link({
+          url: uri,
+          title: title,
+          base_url: req.headers.origin
+        });
+
+        link.save().then(function(newLink) {
+          Links.add(newLink);
+          res.send(200, newLink);
+        });
+      });
+    }
+  });
+});
+
+/************************************************************/
+// Write your authentication routes here
+/************************************************************/
+
+app.get('/signup', function(req, res){
+  res.render('signup');
+});
+
+app.post('/signup', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var user = new User({
+    username: username,
+    password: password
+  });
+
+  user.save().then(function(newUser) {
+    Users.add(newUser);
+    // req.session.regenerate(function(){
+      req.session.user = user.get('username');
+      console.log('req.session.user inside signup', req.session.user);
+      // authenticate(req, res);
+      res.redirect('/');
+    // });
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+  // console.log(User);
+});
+
+app.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+
+  new User({username: username})
+    .fetch()
+    .then(function(user){
+      if(!user){
+        res.redirect('/signup');
+      } else {
+        if(bcrypt.compareSync(password, user.get('password')) === true ){
+            // req.session.regenerate(function(){
+              req.session.user = user.get('username');
+              console.log('req.session.user inside Login', req.session.user);
+              res.redirect('/');
+            // });
+        } else {
+          res.redirect('/login'); // redirect if password/username is wrong
+        }
+        //set session
+      }
+    });
+});
+
+app.post('/logout', function(req, res){
+  req.session.destroy(function(err){
+    console.log(err);
+  });
+  res.redirect('/login');
+});
+
+/************************************************************/
+// Handle the wildcard route last - if all other routes fail
+// assume the route is a short code and try and handle it here.
+// If the short-code doesn't exist, send the user to '/'
+/************************************************************/
+
+app.get('/*', function(req, res) {
+  new Link({ code: req.params[0] }).fetch().then(function(link) {
+    if (!link) {
+      res.redirect('/');
+    } else {
+      var click = new Click({
+        link_id: link.get('id')
+      });
+
+      click.save().then(function() {
+        db.knex('urls')
+          .where('code', '=', link.get('code'))
+          .update({
+            visits: link.get('visits') + 1,
+          }).then(function() {
+            return res.redirect(link.get('url'));
+          });
+      });
+    }
+  });
+});
+
+console.log('Shortly is listening on 4568');
+app.listen(4568);
